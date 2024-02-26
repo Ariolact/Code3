40bdc8f35bbab961b866d0965442ea4c861cbd96
2023VertsekhaBohdan <2023VertsekhaBohdan@DELL-310-13>
2024-02-26 12:13:47 +0100

    Přidejte soubory projektu.

---
+++ Code.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        PersonsAttributes personsAttributes;
+
+        public Person(string name, int age, PersonsAttributes personsAttributes)
+        {
+            this.name = name;
+            this.age = age;
+            this.personsAttributes = personsAttributes;
+        }
+    }
+
+    class PersonsAttributes
+    {
+        int height;
+        int size;
+        int width;
+        public PersonsAttributes(int height, int size, int width)
+        {
+            this.height = height;
+            this.size = size;
+            this.width = width;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonsAttributes attributes = new PersonsAttributes(178, 80, 100);
+            Person person = new Person("Marek", 1, attributes);
+            Console.WriteLine(person.name + "" + person.age);
+            Console.ReadLine();
+        }
+    }
+}
